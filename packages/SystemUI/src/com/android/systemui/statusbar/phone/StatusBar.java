346fceddc0b5f9099f1a5697cb66fe29b7d75d13
ElDainosor <eldainosor@gmail.com>
2018-11-04 08:21:16 +0000

    base: add shishu protostar
    It's like cosmic but greyscaled and without gradient

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2203,6 +2203,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         return ThemeAccentUtils.isUsingShishuCosmosTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
+    public boolean isUsingShishuProtostarTheme() {
+        return ThemeAccentUtils.isUsingShishuProtostarTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
+    }
+
 
     @Nullable
     public View getAmbientIndicationContainer() {
@@ -4033,6 +4037,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         boolean useShishuImmensityTheme = false;
         boolean useShishuAmalgamationTheme = false;
         boolean useShishuCosmosTheme = false;
+        boolean useShishuProtostarTheme = false;
         if (mCurrentTheme == 0) {
             // The system wallpaper defines if QS should be light or dark.
             WallpaperColors systemColors = mColorExtractor
@@ -4048,6 +4053,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             useShishuImmensityTheme = mCurrentTheme == 7;
             useShishuAmalgamationTheme = mCurrentTheme == 8;
             useShishuCosmosTheme = mCurrentTheme == 9;
+            useShishuProtostarTheme = mCurrentTheme == 10;
         }
         if (isUsingDarkTheme() != useDarkTheme) {
             ThemeAccentUtils.setLightDarkTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), useDarkTheme);
@@ -4072,6 +4078,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
         if (isUsingShishuCosmosTheme() != useShishuCosmosTheme) {
             ThemeAccentUtils.setLightShishuCosmosTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), useShishuCosmosTheme);
+        }
+        if (isUsingShishuProtostarTheme() != useShishuProtostarTheme) {
+            ThemeAccentUtils.setLightShishuProtostarTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), useShishuProtostarTheme);
         }
 
         // Lock wallpaper defines the color of the majority of the views, hence we'll use it
