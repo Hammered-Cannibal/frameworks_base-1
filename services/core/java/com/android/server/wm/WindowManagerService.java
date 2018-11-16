f5d35081e490eafdfe5247d989ef6d30990ff66c
Hammered-Cannibal <hammeredcannibal@gmail.com>
2018-11-16 10:15:46 +0000

    Revert "Implement expanded desktop feature"
    This reverts commit 3a79148a2a0f13b8dc3b6e4df0e3e275e3f8490b.

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -3219,11 +3219,6 @@ public class WindowManagerService extends IWindowManager.Stub
         return mPointerEventDispatcher != null;
     }
 
-    @Override
-    public void addSystemUIVisibilityFlag(int flags) {
-        mLastStatusBarVisibility |= flags;
-    }
-
     // Called by window manager policy. Not exposed externally.
     @Override
     public int getLidState() {
